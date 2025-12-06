dde2189c5f08392d25fa2f50ff930c5bee987bf3
Emmanuel Abril <emmanuel.a@evacare.com>
2025-12-06 16:06:14 +0800

    alias react-router

--- vite.cofig.js
+++ vite.cofig.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    base: process.env.VITE_BASE_PATH || "/myportfolio",
-})
- 
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // 👇 Force Vite to use the ESM entry for react-router
+      "react-router": "react-router/dist/index.mjs",
+    },
+  },
+  // For Vercel, base is usually just "/"
+  // If you only care about Vercel, set base: "/"
+  base: process.env.VITE_BASE_PATH || "/",
+});
