871bcd8504a530f26626f05d64c1a7fa9f63c04e
Emmanuel Abril <emmanuel.a@evacare.com>
2025-12-06 15:29:31 +0800

    Install v6 explicitly

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+//import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import SkillsPage from "./pages/SkillsPage";
